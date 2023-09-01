2bcf63f8588f1ec829cccb6d3b28ded394b78c03
Bucha1958 <okoriemmadu@gmail.com>
2023-08-31 19:00:09 -0700

    correcction

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,58 +1,70 @@
 #!/usr/bin/python3
-"""
-0-nqueens.py
-"""
+""" Module For Nqueens problem """
 import sys
 
 
-def queens(n, row=0, p=[], tl=[], tr=[]):
-    """
-    find possible position of queens on a
-    chess board
-    n: size of board (nxn)
-    row: the current row
-    p: current placement of queens
-    tl: diagonals from top-left to bottom-right
-    tr: diagonals from top-right to bottom-left
-    """
-    if row < n:
-        for col in range(n):
-            cond0 = row + col not in tl
-            cond1 = row - col not in tr
-            if col not in p and cond0 and cond1:
-                new_tl = tl + [row + col]
-                new_tr = tr + [row - col]
-                yield from queens(n, row+1, p+[col], new_tl, new_tr)
-    else:
-        yield p
+class NQueens:
+    """ Taking an Nqueen class """
+
+    def __init__(self, n):
+        """ instantiates """
+        self.n = n
+        self.x = [0 for x in range(n + 1)]
+        self.result = []
+
+    def place(self, k, i):
+        """ Checks if k number of  Queen can be placed in i
+            column without being threatebed or another queen
+            in same diagonal
+        """
+
+        # j checks from 1 to k - 1 (Up to previous queen)
+        for j in range(1, k):
+            # If There is already a queen in column in same diagonal
+            if self.x[j] == i or \
+               abs(self.x[j] - i) == abs(j - k):
+                return 0
+        return 1
+
+    def nQueen(self, k):
+        """  possiblity of placing every queen in the board
+        """
+        # moving from column 1 to column n (1st column is 1st index)
+        for i in range(1, self.n + 1):
+            if self.place(k, i):
+                # Queen can be placed in i column
+                self.x[k] = i
+                if k == self.n:
+                    # All 4 Queens placed (A solution was found)
+                    result = []
+                    for i in range(1, self.n + 1):
+                        result.append([i - 1, self.x[i] - 1])
+                    self.result.append(result)
+                else:
+                    # recursion to place more Queens
+                    self.nQueen(k + 1)
+        return self.result
 
 
-def nqueens(n):
-    """
-    solve the N-queens puzzle
-    """
-    result = []
-    i = 0
-    for res in queens(n, 0):
-        for val in res:
-            result.append([i, val])
-            i += 1
-        print(result)
-        result = []
-        i = 0
+# Runs program
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
 
+N = sys.argv[1]
 
-if __name__ == '__main__':
-    if len(sys.argv) > 2 or len(sys.argv) < 2:
-        print("Usage: nqueens N")
-        exit(1)
-    if sys.argv[1].isdigit() is False:
-        print("N must be a number")
-        exit(1)
+try:
+    N = int(N)
+except ValueError:
+    print("N must be a number")
+    sys.exit(1)
 
-    if int(sys.argv[1]) < 4:
-        print("N must be at least 4")
-        exit(1)
+if N < 4:
+    print("N must be at least 4")
+    sys.exit(1)
 
-    n = int(sys.argv[1])
-    nqueens(n)
+queen = NQueen(N)
+result = queen.nQueen(1)
+
+for i in result:
+    print(i)
